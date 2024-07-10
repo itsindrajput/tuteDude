03d4d79be8d9c76a258ea8105494ade8dfde24bf
Rishabh Singh <itsindrajput@gmail.com>
2024-07-10 21:59:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Navbar></Navbar>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>Header</div>
+      <h1>Hello From Header</h1>
+    </>
+  );
+};
+
+export default Header;
